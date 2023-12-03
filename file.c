9d2c692ef592b0a462db0731e023616fef633ff6
wei <809146561@qq.com>
2023-12-03 15:56:22 +0800

    首次的代码提交

---
+++ file.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("xxxxx\n");
+	printf("xxxxx\n");
+	printf("xxxxx\n");
+	printf("xxxxx\n");
+	printf("xxxxx\n");
+	printf("xxxxx\n");
+	printf("xxxxx\n");
+}
